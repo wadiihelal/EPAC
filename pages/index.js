f7e421b3c09a249820523b65a11b544f3c6ecb2a
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2022-07-27 15:13:27 +0100

    activePalletAlert

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,64 @@ import React from "react"
 import Button from 'react-bootstrap/Button';
 import Alerts from '../src/components/alert'
 import 'bootstrap/dist/css/bootstrap.min.css';
-function Home() {   
+import { useState , useEffect } from "react";
+import axios from "axios";
+import {  notification, Space } from 'antd';
+
+function Home ()
+{   
+  const [name, setName] = useState('');  
+  
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([])
+
+  useEffect( () =>
+  {
+    const openNotification = ( nb, zone ) =>
+    {
+  
+      const key = `open${Date.now()}`;
+      const btnclose = (
+        <Button variant='danger' type="primary" size="small" onClick={() => notification.close(key)}>
+          Close
+        </Button>
+      );
+      const btn = (
+        <Button variant='danger' type="primary" size="small" onClick={() => notification.close(key)}>
+          Close
+        </Button>
+      );
+      console.log(data)
+      setLoading(true);
+      console.log(loading)
+
+        notification.open({
+          message: 'Zone Notification',
+          description:
+           `The zone ${zone} gots ${nb} pallets , go check it now !`,
+          className: 'custom-class',
+          btn,
+          btnclose,
+          key,
+        });
+      
+    };
+    const fetchData = async () =>{
+      try {
+        const {data: response} = await axios.get('http://localhost:9090/maxNbPalletActive');
+        setData( response );
+        setLoading(true);
+        openNotification( response.number, response.zone)
+        console.log(response)
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    fetchData();
+    setLoading(true);
+   
+  }, [] );
+
     return (
      <div  className="centre">
        <div style ={{fontFamily: 'lucida grande',fontSize:'75px',color:' #141823',marginLeft:'20%'}}>
@@ -17,7 +74,8 @@ function Home() {
 
              <h1  className='wlc'>
               Consult</h1> 
-            </Button>  
+          </Button>  
+
             </div>
         </div> 
 
@@ -25,3 +83,6 @@ function Home() {
 }
 
 export default Home
+
+
+
