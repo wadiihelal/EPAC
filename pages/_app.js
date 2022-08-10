b0b3411f4650ef65f4b13b29ca47424672c17e3f
Wadii_HELAL <macbookpro@MacBook-Pro-de-Macbook.local>
2022-08-10 16:27:00 +0100

    est

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import '../styles/globals.css';
 import'../styles/App.css'
 import codeabarre from '../src/download.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {  message  } from 'antd';
+import "antd/dist/antd.css" 
 
 const Layout = ({ children }) => <div><Navbar/> {children}</div>;
 
